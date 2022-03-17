292d5003205517ad7710feccf3847dee8032107e
patricio-jp <patitojimenez1610@hotmail.com>
2022-03-17 19:16:47 -0300

    Terminado punto 3.

--- tp1_1.c
+++ tp1_1.c
@@ -2,4 +2,13 @@
 
 void main() {
     printf("Hola mundo\n");
+
+    int num = 16;
+    int *punt = &num;
+
+    printf("Contenido del puntero: %d\n", *punt);
+    printf("Direccion de memoria almacenada por el puntero: %p\n", punt);
+    printf("Direccion de memoria de la variable: %p\n", &num);
+    printf("Direccion de memoria del puntero: %p\n", &punt);
+    printf("Tamaño de memoria utilizado por la variable: %d bytes\n", sizeof(num));
 }
